d07fa7719c1284e97f56f34541f0377dc646aca4
124c4a11 <124c4a11@mail.ru>
2022-06-10 15:04:37 +0300

    Server: init

---
+++ server/src/app.module.ts
@@ -0,0 +1,38 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.env',
+        }),
+      ],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        database: configService.get('POSTGRES_DB'),
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USERNAME'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        synchronize: configService.get('DB_SYNC'),
+        entities: [__dirname + 'dist/**/*.entity.{js,ts}'],
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
+  ],
+})
+export class AppModule {}
