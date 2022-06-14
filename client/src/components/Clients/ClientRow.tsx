a46685ef9328e2c16a98aae8df1c55c531e2c306
124c4a11 <124c4a11@mail.ru>
2022-06-14 21:06:48 +0300

    Client: cascade project delete

--- client/src/components/Clients/ClientRow.tsx
+++ client/src/components/Clients/ClientRow.tsx
@@ -5,6 +5,7 @@ import { FaTrash } from 'react-icons/fa';
 import { IClient } from '../../interfaces/IClient';
 import { DELETE_CLIENT } from '../../graphql/client/client.mutations';
 import { GET_CLIENTS } from '../../graphql/client/client.queries';
+import { GET_PROJECTS } from '../../graphql/project/project.queries';
 
 interface ClientRowProps
   extends DetailedHTMLProps<
@@ -17,20 +18,21 @@ interface ClientRowProps
 export function ClientRow({ client }: ClientRowProps): JSX.Element {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    update(cache, { data: { deleteClient } }) {
-      const clients = cache.readQuery<{ clients: IClient[] }>({
-        query: GET_CLIENTS,
-      })!.clients;
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+    // update(cache, { data: { deleteClient } }) {
+    //   const clients = cache.readQuery<{ clients: IClient[] }>({
+    //     query: GET_CLIENTS,
+    //   })!.clients;
 
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter(
-            (client) => client.id !== (deleteClient as IClient).id,
-          ),
-        },
-      });
-    },
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter(
+    //         (client) => client.id !== (deleteClient as IClient).id,
+    //       ),
+    //     },
+    //   });
+    // },
   });
 
   function removeClient() {
